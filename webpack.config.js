d9dc6987e91cba968fc2baf5849ea5a01a591043
yekn <757072346@qq.com>
2019-07-28 17:22:53 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+function resolve(dir){
+  return path.join(__dirname, '..', dir);
+}
+
+module.exports = {
+  entry: {
+    'main': './src/index',
+  },
+  
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, './dist'),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  externals: { },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+        template: './index.html',
+        chunks:['main']
+    })
+  ]
+}
